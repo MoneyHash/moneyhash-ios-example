5b01c3a3dcbd7db80cb4467809f67dc9f085a7b2
Ahmed Salem Elzeiny <ahmedsalemelzeiny2013@gmail.com>
2022-02-27 10:56:48 +0200

    init commit

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -6,12 +6,48 @@
 //
 
 import UIKit
+import MoneyHash
+import MoneyHashShared
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        DispatchQueue.main.async {
+            MHPaymentHandler.start(
+                on: self,
+                withPaymentId: "z984l9y",
+                withEnvironment: .staging
+            ) { status in
+                
+                
+                
+                switch status {
+                case .error(errors: let errors):
+                    print("errors")
+                case .failed:
+                    print("faild")
+                case .requireExtraAction(actions: let actions):
+                    print("actions")
+                case .success:
+                    print("success")
+                case .cancelled:
+                    print("cancelled")
+
+                case .unknown:
+                    print("unknown")
+
+                @unknown default:
+                    print("unknown")
+
+                }
+            }
+        }
+        
+       
+        
     }
 
 
